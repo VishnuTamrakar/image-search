1a433b12dc598247225610a8bcc1e4f3c0c1e0e6
maheshtamarakar <vishnutamatta142@gmail.com>
2023-11-20 22:14:44 +0530

    second commit

--- src/componets/ImageList.jsx
+++ src/componets/ImageList.jsx
@@ -1,10 +1,9 @@
 import { Component } from "react";
 import { connect } from "react-redux";
 class ImageList extends Component {
-  
-    render() {
-        const { searchResults } = this.props;
-        
+  render() {
+    const { searchResults } = this.props;
+
     return (
       <>
         <div className="container ">
@@ -13,23 +12,28 @@ class ImageList extends Component {
               <h1>Rendom</h1>
               <p className="">40 Image has been found</p>
             </div>
-              <div >
-                <div  className="d-flex flex-wrap gap">
-            {searchResults.map((result) => (
-                  <div key={result.id}  className="card"  style={{ width: "18rem" }}>
-                    <img src={result.urls.small} className="card-img-top" alt="..." />
+            <div>
+              <div className="d-flex flex-wrap gap">
+                {searchResults.map((result) => (
+                  <div
+                    key={result.id}
+                    className="card"
+                    style={{ width: "18rem" }}
+                  >
+                    <img
+                      src={result.urls.small}
+                      className="card-img-top"
+                      alt="..."
+                    />
                   </div>
-                  
-                  
-                  ))}
-                  </div>
+                ))}
               </div>
-             <div className="text-center mt-5">
-                  <button className="btn btn-dark  shadow-lg btn2 ">
-                    Load More
-                  </button>
-                </div>
-            
+            </div>
+            <div className="text-center mt-5">
+              <button className="btn btn-dark  shadow-lg btn2 ">
+                Load More
+              </button>
+            </div>
           </div>
         </div>
       </>
@@ -37,8 +41,7 @@ class ImageList extends Component {
   }
 }
 const mapStateToProps = (state) => ({
-    searchResults: state.searchResults,
-  });
-  
+  searchResults: state.searchResults,
+});
 
 export default connect(mapStateToProps)(ImageList);
